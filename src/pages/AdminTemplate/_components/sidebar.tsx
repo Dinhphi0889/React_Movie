29e484e3e27a25994e0d9295dd75a47b3236011a
dinhphi0899 <dinhphi9c@gmail.com>
2024-05-07 18:30:01 +0700

    update redux-thunk, store

---
+++ src/pages/AdminTemplate/_components/sidebar.tsx
@@ -0,0 +1,22 @@
+import { NavLink } from "react-router-dom"
+import './styleSidebar.css'
+export default function SideBar() {
+    return (
+        <div className="sidebar bg-dark">
+            <ul className="navbar-nav mr-auto">
+                <li className="nav-item">
+                    <NavLink className="nav-link" to=''>Admin</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="add-film">Add Film</NavLink>
+                </li>
+                <li className="nav-item">
+                    <NavLink className="nav-link" to="lists-film">Lists Film</NavLink>
+                </li>
+
+            </ul>
+
+        </div>
+    )
+}
+
